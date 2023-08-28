ebd15ef6579577216a854682d18f4dd7317480b4
nikhilbn <nikhilbn003@gmail.com>
2023-08-28 14:15:38 +0530

    email js added to the form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,39 +1,108 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+
+     // Check for empty fields
+     const emptyFieldErrors = {};
+     if (!form.name) emptyFieldErrors.name = "Name is required";
+     if (!form.email) emptyFieldErrors.email = "Email is required";
+     if (!form.message) emptyFieldErrors.message = "Message is required";
+ 
+     // Update errors state
+     setErrors(emptyFieldErrors);
+ 
+     // If any field is empty, return
+     if (Object.keys(emptyFieldErrors).length > 0) {
+       return;
+     }
+
+    console.log("Form data:", form);
+    emailjs
+      .send(
+        "service_is5waxo",
+        "template_4c2xtcm",
+        {
+          from_name: form.name,
+          to_name: "Nikhil kumar B N",
+          from_email: form.email,
+          to_email: "nikhilbn003@gmail.com",
+          message: form.message,
+        },
+        "Kja_jEsULwg2LCeB1"
+      )
+      .then((response) => {
+        console.log("Email sent successfully!", response);
+        if (response) {
+          alert("Thank you. I will get back to you as soon as possible.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+        alert("Ahh, something went wrong. Please try again.");
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="h-screen" id="contact">
       <div className="flex flex-col md:flex-row" data-aos="fade-up">
-  
-<div className="w-full md:w-1/2 bg-[#d3e1e8] p-10 rounded-3xl ml-4">
-  <div className="md:flex md:flex-col md:items-center mb-4">
-    
-    <img
-      src="/path/to/your/image.png"
-      alt="Contact"
-      className="hidden md:block rounded-full w-20 h-20 mx-auto mb-4"
-    />
+        <div className="w-full md:w-1/2 bg-[#d3e1e8] p-10 rounded-3xl ml-4">
+          <div className="md:flex md:flex-col md:items-center mb-4">
+            <img
+              src="/path/to/your/image.png"
+              alt="Contact"
+              className="hidden md:block rounded-full w-20 h-20 mx-auto mb-4"
+            />
 
-    <div className="text-center">
-      <h2 className="text-2xl text-[#287676] font-bold">Contact Information</h2>
-      <p>Email: </p>
-      <p>Phone: </p>
-    </div>
-  </div>
+            <div className="text-center">
+              <h2 className="text-2xl text-[#287676] font-bold">
+                Contact Information
+              </h2>
+              <p>Email: </p>
+              <p>Phone: </p>
+            </div>
+          </div>
 
-  
-  <img
-    src="/path/to/your/image.png"
-    alt="Contact"
-    className="md:hidden rounded-full w-20 h-20 mx-auto mb-4"
-  />
-</div>
+          <img
+            src="/path/to/your/image.png"
+            alt="Contact"
+            className="md:hidden rounded-full w-20 h-20 mx-auto mb-4"
+          />
+        </div>
 
         <div className="w-full md:w-1/2 p-10" data-aos="fade-up">
           <h2 className="text-2xl text-[#287676] font-bold mb-4">
             Get in Touch
           </h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 htmlFor="name"
@@ -46,9 +115,14 @@ const Contact = () => {
                 type="text"
                 id="name"
                 name="name"
+                value={form.name}
+                onChange={handleInputChange}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-custom-green"
                 placeholder="Your Name"
               />
+              {errors.name && (
+                <p className="text-red-500 mt-1">{errors.name}</p>
+              )}
             </div>
 
             <div className="mb-4">
@@ -62,9 +136,14 @@ const Contact = () => {
                 type="email"
                 id="email"
                 name="email"
+                value={form.email}
+                onChange={handleInputChange}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-custom-green"
                 placeholder="Your Email"
               />
+              {errors.email && (
+                <p className="text-red-500 mt-1">{errors.email}</p>
+              )}
             </div>
             <div className="mb-4">
               <label
@@ -76,10 +155,15 @@ const Contact = () => {
               <textarea
                 id="message"
                 name="message"
+                value={form.message}
+                onChange={handleInputChange}
                 rows="4"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-custom-green"
                 placeholder="Your Message"
               ></textarea>
+              {errors.message && (
+                <p className="text-red-500 mt-1">{errors.message}</p>
+              )}
             </div>
             <button
               type="submit"
